b9db202183e3b55ce60bf0a8fc2f2946359bcf4b
Randsafi <rands068@gmail.com>
2025-04-10 21:21:15 +0200

    add cookie

--- alfajr/settings.py
+++ alfajr/settings.py
@@ -112,6 +112,9 @@ USE_I18N = True
 
 USE_TZ = True
 
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
