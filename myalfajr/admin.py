c3cb07994640ffddbeaddcf0314604ac892272bf
Randsafi <rands068@gmail.com>
2025-04-10 16:28:56 +0200

    creat model&admin

--- myalfajr/admin.py
+++ myalfajr/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Fertilizers,seeds # استورد النماذج التي تريد التسجيل بها
 
-# Register your models here.
+admin.site.register(seeds) # تسجيل نموذج المنتج
+admin.site.register(Fertilizers) # تسجيل نموذج التصنيف
--- myalfajr/models.py
+++ myalfajr/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+x={
+    'winter':'شتوي' ,
+    'summer':'صيفي' ,
+    'm':'موسمي' ,
+    'kilo':'كلوات' ,
+    'flower':'زهور' ,
+}
+class seeds(models.Model):
+    name_s=models.CharField(max_length=225)
+    catgory=models.CharField(choices=x ,default='m')
+    img_s= models.ImageField(upload_to='imge',blank=True, max_length=255)
+
+    def __str__(self):
+        return self.name_s
+    
