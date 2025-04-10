aabc94e5d3f72daf2f021ab1f6add60247e8a82c
Randsafi <rands068@gmail.com>
2025-04-10 16:38:40 +0200

    creat staticfiles

--- alfajr/settings.py
+++ alfajr/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -115,7 +116,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.0/howto/static-files/
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'myproject/static')
+]
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- myalfajr/admin.py
+++ myalfajr/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Fertilizers,seeds # استورد النماذج التي تريد التسجيل بها
+from .models import seeds # استورد النماذج التي تريد التسجيل بها
 
 admin.site.register(seeds) # تسجيل نموذج المنتج
-admin.site.register(Fertilizers) # تسجيل نموذج التصنيف
+
