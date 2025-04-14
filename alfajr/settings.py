82a357ba14f335b476c5ad89d9e1d13f3febc343
Randsafi <rands068@gmail.com>
2025-04-14 22:24:36 +0200

    change in alfajr/urls.py

--- alfajr/settings.py
+++ alfajr/settings.py
@@ -7,6 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-98hdrh!*m3vxadau)bhn-*$@2$w@6v(fd0eim71qy$5@qu&ab='
 
 DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = ['https://alfajrco.up.railway.app']
