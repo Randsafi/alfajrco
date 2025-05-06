419bbe14952c5af81e2b555622558a82fb4506c0
Randsafi <rands068@gmail.com>
2025-05-07 01:23:33 +0200

    change in photo

--- myalfajr/admin.py
+++ myalfajr/admin.py
@@ -4,7 +4,7 @@ from .models import seeds # استورد النماذج التي تريد الت
 class YourModelAdmin(admin.ModelAdmin):
     list_display = ('name_s', 'catgory', 'classification','descript')  # الحقول الظاهرة في القائمة
     # list_filter = ('field1',)                     # إضافة فلتر جانبي
-    #search_fields = ('field1', 'field2')          # إضافة بحث
+    search_fields = ('name_s', 'classification')          # إضافة بحث
 
 admin.site.register(seeds, YourModelAdmin)
 #admin.site.register(seeds) # تسجيل نموذج المنتج
